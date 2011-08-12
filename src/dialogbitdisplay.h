e934e057f4462e07fb0225f00dc77a8fccc27653
christophe.hoel <christophe.hoel@gmail.com>
2011-08-12 12:12:48 +0000

    add length as constructor parameter

--- src/dialogbitdisplay.h
+++ src/dialogbitdisplay.h
@@ -11,12 +11,13 @@ class DialogBitDisplay : public QDialog, private Ui::DialogBitDisplay
     Q_OBJECT
 
 public:
-    explicit DialogBitDisplay(ulong & value, QWidget *parent = 0);
+    explicit DialogBitDisplay(ulong & value, int length, QWidget *parent = 0);
 
 private slots:
     void on_buttonBox_accepted();
 
 private:
+    QList<QRadioButton*> listRadio;
     bitset<32> myBitset;
     ulong *myValue;
 };
